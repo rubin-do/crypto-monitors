7251240ff1b0312aa9859aec3fe761de7d3db853
rubin-do <danyarubin@gmail.com>
2022-07-12 21:54:45 +0300

    Add huobi

--- markets/huobi.go
+++ markets/huobi.go
@@ -1,12 +1,76 @@
 package markets
 
-func huobiRequest(url string) {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
 
+type huobiResponse struct {
+	Data []struct {
+		UID        int    `json:"uid"`
+		UserName   string `json:"userName"`
+		CoinID     int    `json:"coinId"`
+		Currency   int    `json:"currency"`
+		PayMethods []struct {
+			Name string `json:"name"`
+		} `json:"payMethods"`
+		MinTradeLimit string `json:"minTradeLimit"`
+		MaxTradeLimit string `json:"maxTradeLimit"`
+		Price         string `json:"price"`
+		TradeCount    string `json:"tradeCount"`
+	} `json:"data"`
+}
+
+const (
+	huobiDataUrl = "https://otc-api.bitderiv.com/v1/data/trade-market?coinId=2&currency=11&tradeType=%s&currPage=1&payMethod=0&acceptOrder=0&country=&blockType=general&online=1&range=0&amount=&onlyTradable=false"
+)
+
+func huobiRequest(url string, orderType string) huobiResponse {
+	url = fmt.Sprintf(huobiDataUrl, orderType)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var respJson huobiResponse
+	json.NewDecoder(resp.Body).Decode(&respJson)
+
+	return respJson
 }
 
 func MonitorHuobiPrice(orders chan<- Order) {
-
 	for {
+		dataBuy := huobiRequest(huobiDataUrl, "sell")
+		bestOrderBuy := dataBuy.Data[0]
+		buyPrice, _ := strconv.ParseFloat(bestOrderBuy.Price, 64)
 
+		var paymentMethods string
+		for _, method := range bestOrderBuy.PayMethods {
+			if len(paymentMethods) != 0 {
+				paymentMethods += ","
+			}
+
+			paymentMethods += method.Name
+		}
+
+		dataSell := huobiRequest(huobiDataUrl, "buy")
+		bestOrderSell := dataSell.Data[0]
+		sellPrice, _ := strconv.ParseFloat(bestOrderSell.Price, 64)
+
+		orders <- Order{
+			"-",
+			"Huobi",
+			bestOrderBuy.UserName,
+			buyPrice,
+			sellPrice,
+			bestOrderBuy.TradeCount,
+			bestOrderBuy.MinTradeLimit,
+			bestOrderBuy.MaxTradeLimit,
+			paymentMethods,
+			fmt.Sprintf("https://c2c.huobi.com/ru-ru/trader/%d", bestOrderBuy.UID),
+		}
 	}
 }
--- markets/huobi_test.go
+++ markets/huobi_test.go
@@ -3,5 +3,26 @@ package markets
 import "testing"
 
 func TestMonitorHuobiPrice(t *testing.T) {
+	orders := make(chan Order)
 
+	go MonitorHuobiPrice(orders)
+
+	order, ok := <-orders
+	price := order.BuyPrice
+
+	if !ok || price <= 0. || price > 1000. {
+		t.Errorf("Failed getting binance price, got %g!", price)
+	}
+
+	t.Logf("Id: %s\nUsername: %s\nBuyPrice: %g\nSellPrice: %g\nQuantity: %s\nMin: %s\nMax: %s\nPaymentMethods: %s\nUrl: %s\n",
+		order.ItemId,
+		order.SellerName,
+		order.BuyPrice,
+		order.SellPrice,
+		order.Quantity,
+		order.MinAmount,
+		order.MaxAmount,
+		order.PaymentMethods,
+		order.Url,
+	)
 }
